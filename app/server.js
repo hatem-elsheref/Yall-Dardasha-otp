cec56147e2e53806c35391d8e2ba68c2b8f377ca
hatem-elsheref <hatem_mohamed_elsheref@yahoo.com>
2021-05-13 03:09:36 +0200

    add some changes

--- app/server.js
+++ app/server.js
@@ -4,14 +4,14 @@ const express = require('express')
 
 const bodyParser = require('body-parser')
 
-// const otpService = require('./routes/otpRoute')
+const otpService = require('./routes/otpRoute')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// app.use('/auth/otp', otpService)
+app.use('/auth/otp', otpService)
 
 app.get('/me', function (request, response) {
     return response.send('otp service')
