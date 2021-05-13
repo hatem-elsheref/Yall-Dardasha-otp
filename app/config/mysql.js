a8d4b69b5337563d76c83586f9ba1f2b991b14c4
hatem-elsheref <hatem_mohamed_elsheref@yahoo.com>
2021-05-13 03:37:06 +0200

    add some changes

--- app/config/mysql.js
+++ app/config/mysql.js
@@ -4,16 +4,17 @@ const mysqlCredentials = configs.db.mysql
 
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
+const connection = mysql.createConnection('mysql://p2far3f59ryfm287:oz4d4u0fd432ukoa@bmlx3df4ma7r1yh4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/lb05onifh0ulceie')
+// const connection = mysql.createConnection({
 
-    host    : mysqlCredentials.db_host,
-    user    : mysqlCredentials.db_user,
-    password: mysqlCredentials.db_pass,
-    database: mysqlCredentials.db_name,
-    port    : mysqlCredentials.db_port,
-    charset : "utf8"
+//     host    : mysqlCredentials.db_host,
+//     user    : mysqlCredentials.db_user,
+//     password: mysqlCredentials.db_pass,
+//     database: mysqlCredentials.db_name,
+//     port    : mysqlCredentials.db_port,
+//     charset : "utf8"
 
-})
+// })
 
 // connection.connect()
 
