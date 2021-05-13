ac644e576859cf253b169f46a5e6d98825f4dd78
hatem-elsheref <hatem_mohamed_elsheref@yahoo.com>
2021-05-13 03:06:32 +0200

    add some changes

--- app/server.js
+++ app/server.js
@@ -4,16 +4,19 @@ const express = require('express')
 
 const bodyParser = require('body-parser')
 
-const otpService = require('./routes/otpRoute')
+// const otpService = require('./routes/otpRoute')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/auth/otp', otpService)
+// app.use('/auth/otp', otpService)
 
+app.get('/me', function (request, response) {
+    return response.send('otp service')
+})
 
-app.listen(config.app_port, function () {
-    console.log(`application running in ${config.app_url}:${config.app_port}`);
+app.listen(process.env.PORT || 80, function () {
+    console.log(`application running in ${config.app_url}:${process.env.PORT || 80}`);
 });
